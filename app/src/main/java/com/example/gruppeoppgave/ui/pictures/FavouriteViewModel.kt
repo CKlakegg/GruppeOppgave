6b5712b61cdca6c348b5fd9b6fdab732ee595b35
Robin Olsen <82152217+robinolsen93@users.noreply.github.com>
2021-04-08 22:31:59 +0200

    Ny font

--- app/src/main/java/com/example/gruppeoppgave/ui/pictures/FavouriteViewModel.kt
+++ app/src/main/java/com/example/gruppeoppgave/ui/pictures/FavouriteViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.gruppeoppgave.ui.home
+package com.example.gruppeoppgave.ui.pictures
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class HomeViewModel : ViewModel() {
+class FavouriteViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is home Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
