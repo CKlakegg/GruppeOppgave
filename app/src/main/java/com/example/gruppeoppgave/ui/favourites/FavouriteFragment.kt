3db17cfd11b5f6782efcfcb4a199e02b62548f03
CKlakegg <81484840+CKlakegg@users.noreply.github.com>
2021-04-15 13:07:08 +0200

    Fiks av FavButton og Ny classe

--- app/src/main/java/com/example/gruppeoppgave/ui/favourites/FavouriteFragment.kt
+++ app/src/main/java/com/example/gruppeoppgave/ui/favourites/FavouriteFragment.kt
@@ -8,10 +8,14 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.gruppeoppgave.R
 import com.example.gruppeoppgave.view.ImageCard
+import com.example.gruppeoppgave.view.ImageFavouriteCard
+import kotlinx.android.synthetic.main.fragment_favourite.view.*
+import kotlinx.android.synthetic.main.fragment_profile.view.*
 
 class FavouriteFragment : Fragment() {
 
     private lateinit var favouriteViewModel: FavouriteViewModel
+    private lateinit var favouritePictureCard: ImageFavouriteCard
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -21,6 +25,8 @@ class FavouriteFragment : Fragment() {
         favouriteViewModel = ViewModelProvider(this).get(FavouriteViewModel::class.java)
 
         val view = inflater.inflate(R.layout.fragment_favourite, container, false)
+
+        favouritePictureCard.picture_RecyclerView
 
         return view
     }
---
+++ app/src/main/java/com/example/gruppeoppgave/view/ImageFavouriteCard.kt
@@ -0,0 +1,30 @@
+package com.example.gruppeoppgave.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.FrameLayout
+import android.widget.ImageView
+import androidx.appcompat.widget.AppCompatButton
+import com.example.gruppeoppgave.R
+import kotlinx.android.synthetic.main.picture_favourtie_card.view.*
+
+class ImageFavouriteCard(context: Context, attrs: AttributeSet) : FrameLayout(context,attrs) {
+
+    private val imageFavouriteview: ImageView
+    private val favFavouriteButton: AppCompatButton
+
+    init {
+
+        val view: View = LayoutInflater.from(context).inflate(R.layout.picture_favourtie_card, this)
+
+        imageFavouriteview = imageView_favourites
+        favFavouriteButton = fav_favourite_button as AppCompatButton
+
+
+    }
+
+
+
+}
